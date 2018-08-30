ae405278573002943c3bf306477688f74a297e31
Jonathan Hechtbauer <jonathan.hechtbauer@ipa.fraunhofer.de>
2018-08-30 19:18:03 +0200

    Fix pkg name

--- robot_recorder_core/setup.py
+++ robot_recorder_core/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['robot_recorder'],
+    packages=['robot_recorder_core'],
     package_dir={'': 'src'}
 )
 setup(**d)
