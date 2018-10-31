74e05d2f4c769029d6497c89d9b21855c039d8e3
Jonathan Hechtbauer <jonathan.hechtbauer@ipa.fraunhofer.de>
2018-10-31 11:32:06 +0100

    Restructure client

--- recordit/src/recordit/client.py
+++ recordit/src/recordit/client.py
@@ -3,24 +3,18 @@
 import rospy
 from std_srvs.srv import Trigger
 
-def record_func(func, before_save=lambda:None):
-
-  # define services
-  recorder = rospy.get_param('~robot_recorder', '/robot_recorder')
-  rospy.loginfo("Trying to connect to server '" + recorder + "/start' ...")
-  rospy.wait_for_service(recorder + '/start')
-  rospy.loginfo("Connected.")
-  preconfigure_record = rospy.ServiceProxy(recorder + '/preconfigure', Trigger)
-  start_record = rospy.ServiceProxy(recorder + '/start', Trigger)
-  save_record = rospy.ServiceProxy(recorder + '/save', Trigger)
-
-  # record
-  preconfigure_record()
-  rospy.loginfo("Start recorder.")
-  start_record()
-  func() # main function
-  before_save() 
-  save_record()
-  rospy.loginfo("Save recording.")
-  
-  return True
+class Recorder(object):
+  def __init__(self, name = '/robot_recorder'):
+    rospy.loginfo("Trying to connect to server '" + name + "/start' ...")
+    rospy.wait_for_service(name + '/start')
+    rospy.loginfo("Connected.")
+    self.start_record = rospy.ServiceProxy(name + '/start', Trigger)
+    self.save_record = rospy.ServiceProxy(name + '/save', Trigger)
+  def __enter__(self):
+    rospy.loginfo("Start recorder.")
+    self.start_record()
+    return
+  def __exit__(self, type, value, traceback):
+    self.save_record()
+    rospy.loginfo("Save recording.")
+    return
